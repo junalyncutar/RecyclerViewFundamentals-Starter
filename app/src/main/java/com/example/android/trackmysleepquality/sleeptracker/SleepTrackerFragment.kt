13601fb53d63c9039d478db8d606b2621ebcd7aa
cutarj <junalyn.cutar@ddp.kyocera.com>
2020-05-04 23:45:47 +0800

    Android Kotlin Fundamentals 07.3: GridLayout with RecyclerView

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -25,6 +25,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.database.SleepDatabase
 import com.example.android.trackmysleepquality.databinding.FragmentSleepTrackerBinding
@@ -65,27 +66,18 @@ class SleepTrackerFragment : Fragment() {
                 ViewModelProviders.of(
                         this, viewModelFactory).get(SleepTrackerViewModel::class.java)
 
-        val adapter = SleepNightAdapter()
-        // Associate the adapter with the RecyclerView
-        binding.sleepList.adapter = adapter
+        // To use the View Model with data binding, you have to explicitly
+        // give the binding object a reference to it.
+        binding.sleepTrackerViewModel = sleepTrackerViewModel
 
-//        sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
-//            sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
-//                it?.let {
-//                    adapter.data = it
-//                }
-//            })
-//        })
+        val adapter = SleepNightAdapter()
+        binding.sleepList.adapter = adapter
 
         sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
             it?.let {
                 adapter.submitList(it)
             }
         })
-
-        // To use the View Model with data binding, you have to explicitly
-        // give the binding object a reference to it.
-        binding.sleepTrackerViewModel = sleepTrackerViewModel
 
         // Specify the current activity as the lifecycle owner of the binding.
         // This is necessary so that the binding can observe LiveData updates.
@@ -124,6 +116,11 @@ class SleepTrackerFragment : Fragment() {
                 sleepTrackerViewModel.doneNavigating()
             }
         })
+
+        val manager = GridLayoutManager(activity, 3)
+        binding.sleepList.layoutManager = manager
+
+
         return binding.root
     }
 }
